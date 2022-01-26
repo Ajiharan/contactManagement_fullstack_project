eaaec628767aa7d4a6e08d91075666ffa611ede6
Ajiharan <baskaranajiharan1243@gmail.com>
2022-01-26 22:08:56 +0530

    sentry dashboard added

--- contact-management-client/src/index.js
+++ contact-management-client/src/index.js
@@ -5,6 +5,18 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import store from "./state-management/store";
 import { Provider } from "react-redux";
+import * as Sentry from "@sentry/react";
+import { Integrations } from "@sentry/tracing";
+Sentry.init({
+  dsn: "",
+  integrations: [new Integrations.BrowserTracing()],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
+
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
